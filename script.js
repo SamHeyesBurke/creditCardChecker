4cafe77469a558f7e35b4155bd4f648f3d28e30b
Sam Heyes-Burke <heyesburkesamuel@gmail.com>
2022-03-16 08:25:11 +0000

    finished adding style

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 
 
-
-function modulo () {
-    if ((x % 2) ) {
-        return x*2
-    }
-}
-
-
-
 const validateCred = (card) => {
     let cardInverted = card.slice().reverse(); // make a copy of the card and invert the array right to left.
     for (let i=0; i < cardInverted.length; i++) { // check each index and pick out the even numbers
@@ -20,10 +11,14 @@ const validateCred = (card) => {
       }
     }
     let sum = cardInverted.reduce((a, b) => a + b, 0); // add all numbers of the array
-    if (sum % 10 == 0) { // check if the modulus 10 equals zero (luhn algorithm)
-      return "The card is valid!"; // if so return true, card is valid
+
+    if(sum === 0) {
+        return "Invalid 😞";
+    }
+    else if (sum % 10 == 0) { // check if the modulus 10 equals zero (luhn algorithm)
+      return "Valid 🎉"; // if so return true, card is valid
     } else {
-      return "The card is invalid!"; // if not return false, card is invalid
+      return "Invalid 😞"; // if not return false, card is invalid
     }  
   }
 
@@ -33,18 +28,46 @@ const validateCred = (card) => {
 
   function getResult () {
     let result = document.getElementById("creditCard").value;
-    document.getElementById('creditValue').innerHTML = result;
+   
     let arr = Array.from(result.toString()).map(Number);
      document.getElementById('isValid').innerHTML = validateCred(arr);
 
-    let warning = document.getElementById('warningMessage').innerHTML;
 
-    if (arr.includes(NaN) || (arr.length >16 || arr.length <15)) {
-        document.getElementById("warningMessage").innerHTML = " -INVALID  - A credit card is a 15-16 digit number"
+    if (arr.includes(NaN) || (arr.length >16 || arr.length <15) || arr.includes(",")) {
+        document.getElementById("warningMessage").innerHTML = "ERROR. A credit card is a 15-16 digit number"
     } else {
         document.getElementById("warningMessage").innerHTML = ""
 
     }
-  }
+
+    if (document.getElementById('isValid').innerHTML == "Invalid 😞") {
+        document.getElementById('isValid').style.color = "red";
+    } else {
+        document.getElementById('isValid').style.color = "green"
+    }
+
+    let companyNum = ""
+        if(arr[0] === 3) {
+        companyNum = "Amex (American Express)"
+        } else if (arr[0] === 4) {
+            companyNum = "Visa"
+        } else if (arr[0] === 5) {
+            companyNum = "Mastercard"
+        } else if (arr[0] === 5) {
+            companyNum = "Discover"
+        } else {
+            companyNum = "Company not found"
+        }
+
+    document.getElementById("companyName").innerHTML = companyNum;
+
+    if (companyNum === "Company not found") {
+      document.getElementById("companyName").style.color = "red";
+    } else {
+      document.getElementById("companyName").style.color = "green"
+    }
+
+    }
+  
 
 
